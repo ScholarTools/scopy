6961a362fd1e87dbc32db5607383ad41bd85b037
ksarmentrout <k.armentrout11@gmail.com>
2016-06-27 16:53:20 -0400

    merged

--- scopy_test_code.py
+++ scopy_test_code.py
@@ -13,9 +13,6 @@ eid_search = api.search('eid(%s)' % first.eid)
 eid_bib = api.bibliography_retrieval.get_from_eid(first.eid)
 
 
-import pdb
-pdb.set_trace()
-
 results = api.search('PMID(3806812)')
 
 doc = results.entries[0]
@@ -30,7 +27,7 @@ print(abs)
 #refs = api.bibliography_retrieval.get_from_doi(doi)
 
 pubmed_id = '11826063'
-pubmed_id = '3806812' #Doesn't work! perhaps via search?
+#pubmed_id = '3806812' #Doesn't work! perhaps via search?
 
 try:
     pubmed_refs = api.bibliography_retrieval.get_from_pubmed(pubmed_id)
